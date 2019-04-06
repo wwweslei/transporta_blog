6407097346d4ea77a08ccd5bde698c37ab09b792
Weslei Silva Bomfim <www.weslei@gmail.com>
2019-04-06 09:20:32 -0300

    customization

--- transporta_blog/settings.py
+++ transporta_blog/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*', '34.220.99.32',
                  'ec2-34-220-99-32.us-west-2.compute.amazonaws.com']
 
+ADMIN_SITE_HEADER = "Transporta.club Administração"
 
 # Application definition
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
